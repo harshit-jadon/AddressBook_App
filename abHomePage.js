73baedf7463d8fad08b5108e105875afbf1f591d
harshit.jadon <harshit.jadon@MD-CJDKE06555KH.magicsw.com>
2021-07-20 01:35:46 +0530

    remove a contact from address book

--- abHomePage.js
+++ abHomePage.js
@@ -23,11 +23,21 @@ const createInnerHtml = () => {
           <td>${personData._zipCode}</td>
           <td>${personData._phoneNumber}</td>
           <td>
-          <img src="delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
-          <img src="create-black-18dp.svg" alt ="update" id="1" onclick="update(this)">
+          <img id="${personData._id}" src="delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
+          <img id="${personData._id}" src="create-black-18dp.svg" alt ="update" id="1" onclick="update(this)">
           </td>
         </tr>
        `;
     }    
     document.querySelector('#display').innerHTML = innerHtml;
+} 
+
+const remove = (node) => {
+    let personData = personDataList.find(person => person._id == node.id);
+    if(!personData) return;
+    const index = personDataList.map(person => person._id)
+                                .indexOf(personData._id);
+    personDataList.splice(index,1);
+    localStorage.setItem("AddressBookList",JSON.stringify(personList));
+    createInnerHtml();
 } 
